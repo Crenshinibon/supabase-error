9d54381ff71fc990446e5cb0f0b01185e09693a3
Dirk Porsche <d.porsche@verband.creditreform.de>
2022-10-26 10:04:07 +0200

    rm node_modules

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -10,11 +10,18 @@
 <h1>CONNECTING</h1>
 {:then client}
 <h1>SUCCESS</h1>
+<form method="POST">
+  <input type="text" name="something" value="">
+  <button>Submit</button>
+</form>
+
 {@debug client}
 {:catch error}
 <h1>ERROR</h1>
 {@debug error}
 {/await}
+
+
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
